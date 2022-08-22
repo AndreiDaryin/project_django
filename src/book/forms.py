31ba95c76138730ceb20b0385a84d4eabc278a8c
Andrei_Daryin <darin2d@mail.ru>
2022-08-22 14:52:56 +0300

    addbooks

--- src/book/forms.py
+++ src/book/forms.py
@@ -5,3 +5,7 @@ class AddBookForm(forms.ModelForm):
     class Meta:
         model = models.Book
         fields = '__all__'
+
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file = forms.FileField()
